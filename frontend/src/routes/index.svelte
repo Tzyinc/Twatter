9a7c1560ab3fbe93d78915dec191d33f5d3a13a6
Tzyinc <tzyinc@hotmail.com>
2024-12-27 20:54:06 +0800

    added tracking

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -9,6 +9,8 @@
 
 <!-- src/routes/blog/[slug].svelte -->
 <svelte:head>
+	<script defer src="https://umami.home.tenzhiyang.com/script.js"
+		data-website-id="a86bb1bd-a315-43bc-b1ee-06117a1d4a0c"></script>
   <title>Twatter</title>
 </svelte:head>
 <Compose />
